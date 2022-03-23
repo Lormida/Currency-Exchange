2ef47cc229cec0697c2b071c2cf14b16d1b4958f
Lormida <pocketbook.love24@gmail.com>
2022-03-24 00:07:48 +0300

    Animation transition between pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="page" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <!-- Modal buy-->
 
   <ModalWrapper v-if="getIsModalOpen"></ModalWrapper>
@@ -11,7 +15,7 @@ import ModalWrapper from '@/components/UI/ModalWrapper.vue';
 import { useStore } from '@/store'
 import { computed, defineComponent } from 'vue';
 export default defineComponent({
-  components: { ModalWrapper},
+  components: { ModalWrapper },
   setup() {
     const store = useStore()
     const getIsModalOpen = computed(() => store.getters.getIsModalOpen)
@@ -23,5 +27,24 @@ export default defineComponent({
 * {
   font-family: "Inter", sans-serif;
   box-sizing: border-box;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+.page-enter-to,
+.page-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
+}
+.page-enter-active {
+  transition: all 0.5s ease-in-out 0s;
+}
+.page-leave-active {
+  transition: all 0.25s ease-in-out 0s;
+}
+html {
+  background-color: #9a8c98;
 }
 </style>
